5f929fd2749280c46b6fd4ad335fb0f9ab5adac5
Evgenii Igoshin <johnshin378@gmail.com>
2024-06-17 00:59:27 +0200

    modified folders - removed folder - src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     screens:{
       maxlg:{'max': '1919.99px'},
